77747f90e3dc9e72308b00109787a4a017f402e1
Mohitraj27 <raj716980@gmail.com>
2025-06-11 11:32:28 +0530

    Common Rate Limiter inside gateway

--- gateway/app.js
+++ gateway/app.js
@@ -1,9 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const expressProxy = require('express-http-proxy');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
-
+const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 mins
+    max: 30, // Limit each IP to 10 requests per windowMs
+    message: {
+        message: 'Too many attempts, please try again after 15 minutes.'
+    },
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+app.use(authLimiter);
 app.use('/user', expressProxy(process.env.BASE_URL_USER));
 app.use('/captain', expressProxy(process.env.BASE_URL_CAPTAIN));
 app.use('/ride', expressProxy(process.env.BASE_URL_RIDE));
